650619f32aa2fca46f0662140513463f0133ad93
junwufan <shelllet.com@gmail.com>
2021-09-01 09:34:17 +0800

    Add project files.

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+
+namespace ClassLibrary1
+{
+    public sealed class Class1
+    {
+        public void Test(object arg1)
+        {
+            var iis = (WinRT.IInspectable)arg1;
+
+            var name = iis.GetRuntimeClassName();
+
+            var list = iis.As<IObservableVector<int>>();
+        }
+    }
+}
---
+++ WinRT_IInspectable/WinRT_IInspectable/App.xaml.h
@@ -0,0 +1,16 @@
+﻿#pragma once
+
+#include "App.xaml.g.h"
+
+namespace winrt::WinRT_IInspectable::implementation
+{
+    struct App : AppT<App>
+    {
+        App();
+
+        void OnLaunched(Microsoft::UI::Xaml::LaunchActivatedEventArgs const&);
+
+    private:
+        winrt::Microsoft::UI::Xaml::Window window{ nullptr };
+    };
+}
